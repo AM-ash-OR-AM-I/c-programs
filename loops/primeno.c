b73f957c52a9d2f779d8cde5af6df44e417a2296
Ashutosh Mahapatra <ashutosh2gudu@gmail.com>
2023-09-28 09:23:26 +0530

    added code

--- loops/primeno.c
+++ loops/primeno.c
@@ -1,23 +1,57 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
+#include <string.h>
+
+bool isPrime(int n)
+{
+  for (int j = 2; j * j <= n; j++)
+  {
+    if ((n % j) == 0)
+      return false;
+  }
+  return true;
+}
+
+bool checkPrimesOptimized(int n)
+{
+  bool prime[n + 1];
+  int primeCount = 0;
+  memset(prime, true, sizeof(prime));
+  for (int p = 2; p * p <= n; p++)
+  {
+    if (prime[p])
+    {
+      for (int i = p * p; i <= n; i += p)
+        prime[i] = false;
+    }
+  }
+  for (int p = 2; p <= n; p++)
+  {
+    if (prime[p])
+      primeCount++;
+  }
+  printf("Count of primes = %d", primeCount);
+}
+
+void checkPrimes(int n)
+{
+  int primeCount = 0;
+  for (int i = 2; i < n; i++)
+  {
+    bool prime = isPrime(i);
+    if (prime)
+      primeCount++;
+    // printf("%d is %sprime\n", i, (prime ? "" : "not "));
+  }
+  printf("Count of primes = %d", primeCount);
+}
 
 void main()
 {
   int n;
-  printf("Enter the number n: ");
+  printf("Enter the range n: ");
   scanf("%d", &n);
-  for (int i = 2; i < n; i++)
-  {
-    bool isPrime = true;
-    for (int j = 2; j * j < n; j++)
-    {
-      if ((i % j) == 0)
-      {
-        isPrime = false;
-        break;
-      }
-    }
-    printf("%d is %s", i, (isPrime ? "prime\n" : "not prime\n"));
-  }
+  // checkPrimes(n);
+  checkPrimesOptimized(n);
 }
---
+++ loops/reverse-and-pallindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool checkPallindrome(int n)
+{
+  int rev = 0, orgN = n;
+  while (n)
+  {
+    int rem = n % 10;
+    rev = rev * 10 + rem;
+    n /= 10;
+  }
+  return orgN == rev;
+}
+
+int main()
+{
+  int n;
+  printf("Enter the number: ");
+  scanf("%d", &n);
+  bool pallindrome = checkPallindrome(n);
+  printf("%d is %spallindrome", n, pallindrome ? "" : "not ");
+}
