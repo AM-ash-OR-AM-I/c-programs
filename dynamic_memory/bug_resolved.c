21a2fd379fd357f2f1a4281a99d1ac46e556e2ca
Ashutosh Mahapatra <ashutosh2gudu@gmail.com>
2023-11-08 22:58:34 +0530

    fixed bug

--- dynamic_memory/bug_resolved.c
+++ dynamic_memory/bug_resolved.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int LENGTH = 6;
+int LENGTH = 3;
 
-void arrayAddition(int *ptr, int index) {
+const int INCREMENT_LENGTH = 5;
+
+int *arrayAddition(int *ptr, int index) {
   if (index >= LENGTH) {
     printf("Reallocating\n");
-    LENGTH += 10;
+    LENGTH += INCREMENT_LENGTH;
     ptr = (int *)realloc(ptr, LENGTH * sizeof(int)); // Reallocation
   }
-  printf("Enter %dth element: ", index + 1);
   *(ptr + index) = index;
   printf("Memory: %p, Value: %d\n", (ptr + index), *(ptr + index));
+  return ptr;
 }
 
 int main() {
   int *arr = (int *)malloc(LENGTH * sizeof(int));
   int n = 16;
   for (int i = 0; i < n; i++) {
-    arrayAddition(arr, i);
+    arr = arrayAddition(arr, i);
   }
 
   printf("Elements of arr are: \n");
