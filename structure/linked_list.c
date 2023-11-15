e73f19efe434af7fd04b7d9dd7ba4d8e131d5ae6
Ashutosh Mahapatra <ashutosh2gudu@gmail.com>
2023-11-15 13:47:37 +0530

    updated code

--- structure/linked_list.c
+++ structure/linked_list.c
@@ -3,31 +3,37 @@
 
 struct node {
   int data;
-  struct node *link;
+  struct node *nextNode;
 };
 
-int main() {
-  struct node start;
-  start.data = 324;
-  start.link = NULL;
-  struct node *curNode = &start;
+void displayLinkedList(struct node *curNode) {
+  printf("LinkedList: \n");
+  while (curNode->nextNode != NULL) {
+    printf("%d -> ", curNode->data);
+    curNode = curNode->nextNode;
+  }
+  printf("%d", curNode->data);
+}
 
-  for (int i = 0; i < 10; i++) {
+int main() {
+  printf("Enter length of linkedlist: ");
+  int length;
+  scanf("%d", &length);
+
+  printf("Enter elements of linkedlist: ");
+  struct node start;
+  scanf("%d", &start.data);
+  start.nextNode = NULL;
+  struct node *curNode = &start;
+  for (int i = 0; i < length - 1; i++) {
     struct node *newNode = malloc(sizeof(int) + 4);
-    newNode->data = rand() % 255;
-    newNode->link = NULL;
-    curNode->link = newNode;
-    printf("Before assigning: %p %d -> %p\n", curNode, curNode->data,
-           curNode->link);
+    scanf("%d", &newNode->data);
+    newNode->nextNode = NULL;
+    curNode->nextNode = newNode;
+    // printf("Before assigning: %p %d -> %p\n", curNode, curNode->data,
+    //        curNode->link);
     curNode = newNode;
   }
 
-  curNode = &start;
-
-  printf("LinkedList: \n");
-  while (curNode->link != NULL) {
-    printf("%d -> ", curNode->data);
-    curNode = curNode->link;
-  }
-  printf("%d", curNode->data);
+  displayLinkedList(&start);
 }
