318d1c4b90d6761403e340c54f6161807b9c2198
Ashutosh Mahapatra <ashutosh2gudu@gmail.com>
2023-12-14 17:53:14 +0530

    updated q16

--- ASSIGNMENTS/assign-6/q16_easy.c
+++ ASSIGNMENTS/assign-6/q16_easy.c
@@ -1,9 +1,3 @@
-/*
-Simulate the following structure in C to find the element sum of the given
-arrays a, b, c and d into sumarray using their respective pointers. The 1-D
-arrays must be read/scanned through the pointers.
-*/
-
 #include "../../utils/input.c"
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,22 +8,19 @@ int main() {
   scanf("%d", &n);
   int sumArray[n];
   int *pointerSum = sumArray;
-  int **arrayPointers = malloc(sizeof(int *) * 4);
-  for (int i = 0; i < 4; i++) {
-    arrayPointers[i] = malloc(sizeof(int) * n);
-  }
+  int arrayPointers[4][n];
 
   for (int i = 0; i < 4; i++) {
     printf("Enter elements of arr%d(%c): ", i, 97 + i);
     for (int j = 0; j < n; j++) {
-      scanf("%d", &(*(*(arrayPointers + i) + j)));
+      scanf("%d", &arrayPointers[i][j]);
     }
   }
 
   for (int i = 0; i < n; i++) {
     *pointerSum = 0;
     for (int j = 0; j < 4; j++) {
-      *pointerSum += *(*(arrayPointers + j) + i);
+      *pointerSum += arrayPointers[j][i];
     }
     pointerSum++;
   }
